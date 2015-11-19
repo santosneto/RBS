08418eb54a3efd15bbcfcf3891c71416889fe4fe
santosneto <santosneto@MacBook-Pro-de-Manoel.local>
2015-11-19 17:09:48 -0300

    update of datasets

--- R/bs2.R
+++ R/bs2.R
@@ -329,3 +329,135 @@ rcbs <- function(n, alpha = 1, beta = 1)
   return(t)
 }
 
+#'Brown and Miller's biaxial fatigue data
+#'
+#'Several data sets related to the BS distribution are available in the RBS package, which have been taken from the literature on this topic.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name biaxial
+#'@usage data(biaxial)
+#'@format A vector containing 46 observations.
+#'@details Rieck and Nedelman (1991) reported a data set from Brown and Miller (1978). These data represent results of fatigue tests on 1 \eqn{\%} Cr-Mo-V steel. Cylindrical specimens were subjected to combined torsional and axial loads over constant-amplitude cycles until failure.
+#'
+#'@references
+#'
+#'Rieck, J. R. and Nedelman, J. (1991). A Log-Linear Model for the Birnbaum-Saunders Distribution. Technometrics. 33, 51-60.
+#'
+#'Brown, M. W. and Miller, K. J. (1978). Biaxial Fatigue Data. Report CEMR1/78. University of Sheffield, Dept. of Mechanical Engineering.
+#'
+#'@examples
+#'## Load data sets
+#'data(biaxial)
+#'## Histogram for biaxial
+#'hist(biaxial)
+#'
+NULL
+
+
+#'Fatigue lifetime data
+#'
+#'Several data sets related to the BS distribution are available in the RBS package, which have been taken from the literature on this topic.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name bearings
+#'@usage data(bearings)
+#'@format A vector containing 10 observations.
+#'@details McCool (1974) gives data related to the fatigue life in hours of 10 bearings of a certain type.
+#'
+#'@references
+#'
+#'McCool, J. I. (1974). Inferential techniques for Weibull populations. Aerospace Research Laboratories Report ARL TR74-0180, Wright-Patterson Air Force Base, Dayton, OH.
+#'
+#'@examples
+#'## Load data sets
+#'data(bearings)
+#'## Histogram for bearings
+#'hist(bearings)
+#'
+NULL
+
+
+#'Lifetimes of aluminum specimens exposed to a maximum stress of 21,000 psi.
+#'
+#'Several data sets related to the BS distribution are available in the RBS package, which have been taken from the literature on this topic.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name psi21
+#'@usage data(psi21)
+#'@format A vector containing 101 observations.
+#'@details psi21, psi26 and psi31 were taken from Birnbaum and Saunders (1969),
+#'who reported fatigue life data correspond to the cycles (\eqn{\times 10^{-3}}) of aluminum
+#'specimens of type 6061-T6. These specimens were cut in a parallel angle to the direction of
+#'rotation and oscillating at 18 cycles per seconds. They were exposed to a pressure with maximum
+#'stress of 21,000, 26,000, and 31,000 psi (pounds per square inch), for \eqn{n = 101, 102,}
+#'and \eqn{101} specimens, respectively.
+#'
+#'@references
+#'Birnbaum, Z. W. and Saunders, S. C. (1969). Estimation for a family of life distributions with applications to fatigue. J. Appl. Probab. 6(2): 328-347.
+#'
+#'@examples
+#'## Load data sets
+#'data(psi21)
+#'## Histogram for psi21
+#'hist(psi21)
+#'
+NULL
+
+#'Lifetimes of aluminum specimens exposed to a maximum stress of 26,000 psi.
+#'
+#'Several data sets related to the BS distribution are available in the RBS package, which have been taken from the literature on this topic.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name psi26
+#'@usage data(psi26)
+#'@format A vector containing 102 observations.
+#'@details psi21, psi26 and psi31 were taken from Birnbaum and Saunders (1969),
+#'who reported fatigue life data correspond to the cycles (\eqn{\times 10^{-3}}) of aluminum
+#'specimens of type 6061-T6. These specimens were cut in a parallel angle to the direction of
+#'rotation and oscillating at 18 cycles per seconds. They were exposed to a pressure with maximum
+#'stress of 21,000, 26,000, and 31,000 psi (pounds per square inch), for \eqn{n = 101, 102,}
+#'and \eqn{101} specimens, respectively.
+#'
+#'@references
+#'Birnbaum, Z. W. and Saunders, S. C. (1969). Estimation for a family of life distributions with applications to fatigue. J. Appl. Probab. 6(2): 328-347.
+#'
+#'@examples
+#'## Load data sets
+#'data(psi26)
+#'## Histogram for psi26
+#'hist(psi26)
+#'
+NULL
+
+
+#'Lifetimes of aluminum specimens exposed to a maximum stress of 31,000 psi.
+#'
+#'Several data sets related to the BS distribution are available in the RBS package, which have been taken from the literature on this topic.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name psi31
+#'@usage data(psi31)
+#'@format A vector containing 101 observations.
+#'@details psi21, psi26 and psi31 were taken from Birnbaum and Saunders (1969),
+#'who reported fatigue life data correspond to the cycles (\eqn{\times 10^{-3}}) of aluminum
+#'specimens of type 6061-T6. These specimens were cut in a parallel angle to the direction of
+#'rotation and oscillating at 18 cycles per seconds. They were exposed to a pressure with maximum
+#'stress of 21,000, 26,000, and 31,000 psi (pounds per square inch), for \eqn{n = 101, 102,}
+#'and \eqn{101} specimens, respectively.
+#'
+#'@references
+#'Birnbaum, Z. W. and Saunders, S. C. (1969). Estimation for a family of life distributions with applications to fatigue. J. Appl. Probab. 6(2): 328-347.
+#'
+#'@examples
+#'## Load data sets
+#'data(psi31)
+#'## Histogram for psi31
+#'hist(psi31)
+#'
+NULL
+
