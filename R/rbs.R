1094942bf1e8b901e3e8d512a27dd64c1e1794bd
santosneto <mn.neco@gmail.com>
2020-03-07 15:29:18 -0300

    update

--- R/rbs.R
+++ R/rbs.R
@@ -456,8 +456,8 @@ RBS <- function (mu.link = "log" , sigma.link="log")
 
                  G.dev.incr = function(y,mu,sigma,...) -2*dRBS(y,mu,sigma,log=TRUE),
                  rqres = expression(rqres(pfun = "pRBS", type = "Continuous", y = y, mu = mu, sigma = sigma)),
-                 mu.initial = expression({mu <- y + 0.01 }),
-                 sigma.initial = expression({sigma = rep(sigmatil(y),length(y))}),
+                 mu.initial = expression({mu <- y + mean(y)/2 }),
+                 sigma.initial = expression({sigma <- rep(1,length(y)) }),
                  mu.valid = function(mu) all(mu>0) ,
                  sigma.valid = function(sigma) all(sigma > 0),
                  y.valid = function(y) all(y > 0),
