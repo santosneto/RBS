6ae6b6422c3a696bc83ffcf46bd5cd7c3079f189
santosneto <mn.neco@gmail.com>
2020-04-23 16:37:43 -0300

    update

--- R/rbs.R
+++ R/rbs.R
@@ -132,8 +132,7 @@ rcbs <- function(n, alpha = 1, beta = 1)
   if (alpha <= 0){stop("alpha must be positive")}
   if (beta <= 0){stop("beta must be positive")}
   z   <- rnorm(n, 0, 1)
-  t   <- beta * (1 + (((alpha^2) * (z^2)) / 2) +
-                   (alpha * z *sqrt((((alpha^2) * (z^2))/ 4)+ 1)))
+  t   <- (beta/4)*(alpha*z + sqrt((alpha*z)^2 + 4))^2
   return(t)
 }
 
